c7fc8f73191bbe772d4c6eeb0ca07fac8e272a78
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-12 01:40:29 -0400

    se deja solo ruta principal

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 const express = require("express");
 const cors = require("cors");
 const _ = require("lodash");
-const { v4: uuid } = require("uuid");
+
 
 const app = express();
 app.use(express.json());
@@ -20,36 +20,6 @@ app.get("/outfit", (req, res) => {
   });
 });
 
-app.get('/comments/:id', async (req, res) =>{
-  const id = req.params.id
-  let content
 
-  try{
-    content = await fs.readFile(`data/comments/${id}.txt`, 'utf-8')
-  } catch (err) {
-    return res.sendStatus(404)
-  }
-
-  res.json({
-    content: content
-  })
-
-})
-
-app.post("/comments", async (req, res) => {
-  const id = uuid();
-  const content = req.body.content
-  
-  if(!content) {
-    return res.sendStatus(400)
-  }
-
-  await fs.mkdir('data/comments', {recursive: true}) 
-  await fs.writeFile(`data/comments/${id}.txt`, content) 
-
-  res.status(201).json({
-    id: id
-  });
-});
 
 app.listen(3000, () => console.log("API server is running"));
